47b9b42ee653a4abf12a6b24bee048925eda6e23
Konstantin Shutkin <kostya80@gmail.com>
2021-12-29 23:04:27 +0400

    fix: write new line at the end of package.json

--- src/write-pkg.ts
+++ src/write-pkg.ts
@@ -2,5 +2,5 @@ import fs from 'fs/promises';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function writePackage(pkgPath: string, pkg: any) {
-    await fs.writeFile(pkgPath, JSON.stringify(pkg, null, 2));
+    await fs.writeFile(pkgPath, JSON.stringify(pkg, null, 2) + '\n');
 }
