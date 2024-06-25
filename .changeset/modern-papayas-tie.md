7b853116abcc8a0bb7d573db1db58444b73e9195
Konstantin Shutkin <kostya80@gmail.com>
2024-06-25 23:39:42 +0200

    fix: update commonjs plugin

---
+++ .changeset/modern-papayas-tie.md
@@ -0,0 +1,7 @@
+---
+"cli-test-helper": patch
+"pkgbld-internal": patch
+"pkgbld": patch
+---
+
+update commonjs plugin
--- cli-test-helper/index.d.ts
+++ cli-test-helper/index.d.ts
@@ -2,11 +2,13 @@ declare module 'cli-test-helper' {
 	/**
 	 * Converts a string to real files and directories in the file system.
 	 * */
-	export function stringToFiles(data: string, baseDir: string): Promise<void>;
+	function stringToFiles(data: string, baseDir: string): Promise<void>;
 	/**
 	 * Converts files and directories in the file system to a string.
 	 * */
-	export function filesToString(baseDir: string, ignore?: string[] | undefined): Promise<string>;
+	function filesToString(baseDir: string, ignore?: string[] | undefined): Promise<string>;
+
+	export { stringToFiles, filesToString };
 }
 
 //# sourceMappingURL=index.d.ts.map
